b0ad370e227b77d7094fdad75fc13d92d3d61ed3
Mahdi <mkfard@outlook.com>
2026-02-24 09:13:50 +0330

    Release version 0.0.2.

--- lib/gpu.dart
+++ lib/gpu.dart
@@ -1,5 +1,7 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import "package:flutter_gpu/gpu.dart" as gpu;
+
+gpu.PixelFormat get defaultColorFormat => gpu.gpuContext.defaultColorFormat;
+
+gpu.ShaderLibrary loadShaderLibrary(String assetName) {
+  return gpu.ShaderLibrary.fromAsset(assetName)!;
 }
--- test/gpu_test.dart
+++ test/gpu_test.dart
@@ -1,12 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:gpu/gpu.dart';
+import "package:flutter_gpu/gpu.dart" as gpu;
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('Checks default color format.', () {
+    final x = gpu.BlendOperation.add;
+
+    expect(x, gpu.BlendOperation.add);
   });
 }
